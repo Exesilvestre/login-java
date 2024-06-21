babf69283f6a12fc20f1dc24b685d6c1beb5eca7
Exesilvestre <exesilvestre@gmail.com>
2024-06-21 20:19:38 -0300

    users entity

--- src/main/java/challenge/demo/Entities/User.java
+++ src/main/java/challenge/demo/Entities/User.java
@@ -1,4 +1,31 @@
 package challenge.demo.Entities;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Table(name = "users")
+@Data
+@Entity
 public class User {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String idUser;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
 }
